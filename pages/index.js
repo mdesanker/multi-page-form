46f13a6bee74a0e1ab66dc5b2aaf8137be7a7379
Michael Desanker <mdesanker@gmail.com>
2022-04-05 19:20:22 -0400

    Add head element

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from "next/head";
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>Multi-Page Form</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1>Hello, world!</h1>
     </div>
   );
