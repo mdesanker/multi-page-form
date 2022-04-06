98eaa3158f5b3de60aaad47a5a0dc9cd99c98970
Michael Desanker <mdesanker@gmail.com>
2022-04-05 20:11:04 -0400

    Add create bounty btn to index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
+import { Button, Flex } from "@chakra-ui/react";
 import Head from "next/head";
+import NextLink from "next/link";
 
 const Home = () => {
   return (
-    <div>
+    <Flex justifyContent="center" alignItems="center" h="50vh">
       <Head>
         <title>Multi-Page Form</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1>Hello, world!</h1>
-    </div>
+      <NextLink href="#" passHref>
+        <Button size="lg" colorScheme="blue" variant="outline">
+          Create a Bounty
+        </Button>
+      </NextLink>
+    </Flex>
   );
 };
 
