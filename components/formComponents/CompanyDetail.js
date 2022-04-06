792266f0cfb5a360d537a7f7e44f2ec2704f8969
Michael Desanker <mdesanker@gmail.com>
2022-04-06 04:04:34 -0400

    Add validation with yup

--- components/formComponents/CompanyDetail.js
+++ components/formComponents/CompanyDetail.js
@@ -1,6 +1,7 @@
 import {
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Input,
@@ -8,6 +9,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const CompanyDetail = () => {
   const formik = useFormik({
@@ -21,6 +23,18 @@ const CompanyDetail = () => {
       admin: "",
       email: "",
     },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Company name is required"),
+      location: Yup.string().required("Location is required"),
+      policy: Yup.string().required("Remote policy is required"),
+      size: Yup.string().required("Company size is required"),
+      stage: Yup.string().required("Funding stage is required"),
+      url: Yup.string().required("Company URL is required"),
+      admin: Yup.string().required("Admin name is required"),
+      email: Yup.string()
+        .email("Invalid eail address")
+        .required("Admin email address is required"),
+    }),
     onSubmit: (values, actions) => {
       alert(JSON.stringify(values, null, 2));
       // actions.resetForm();
@@ -33,45 +47,54 @@ const CompanyDetail = () => {
         Company Details
       </Heading>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.name && formik.touched.name}>
         <FormLabel>Company Name</FormLabel>
         <Input
           name="name"
           value={formik.values.name}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl
+        isInvalid={formik.errors.location && formik.touched.location}
+      >
         <FormLabel>Location</FormLabel>
         <Input
           name="location"
           value={formik.values.location}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        <FormErrorMessage>{formik.errors.location}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.policy && formik.touched.policy}>
         <FormLabel>Remote policy</FormLabel>
         <Select
           name="policy"
           placeholder="Select option"
           value={formik.values.policy}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         >
           <option value="remote">Remote</option>
           <option value="hybrid">Hybrid</option>
           <option value="onsite">On-site</option>
         </Select>
+        <FormErrorMessage>{formik.errors.policy}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.size && formik.touched.size}>
         <FormLabel>Number of employees</FormLabel>
         <Select
           name="size"
           placeholder="Select option"
           value={formik.values.size}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         >
           <option value="xs">{`< 10`}</option>
           <option value="sm">{`11 - 100`}</option>
@@ -79,47 +102,55 @@ const CompanyDetail = () => {
           <option value="lg">{`101 - 500`}</option>
           <option value="xl">{`500+`}</option>
         </Select>
+        <FormErrorMessage>{formik.errors.size}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.stage && formik.touched.stage}>
         <FormLabel>Funding Stage</FormLabel>
         <Select
           name="stage"
           placeholder="Select option"
           value={formik.values.stage}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         >
           <option value="pre">Pre-seed</option>
           <option value="seed">Seed</option>
           <option value="series">Series A/B/C</option>
         </Select>
+        <FormErrorMessage>{formik.errors.stage}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.url && formik.touched.url}>
         <FormLabel>Company Website</FormLabel>
         <Input
           name="url"
           value={formik.values.url}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.admin && formik.touched.admin}>
         <FormLabel>Admin Name</FormLabel>
         <Input
           name="admin"
           value={formik.values.admin}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        <FormErrorMessage>{formik.errors.admin}</FormErrorMessage>
       </FormControl>
 
-      <FormControl>
+      <FormControl isInvalid={formik.errors.email && formik.touched.email}>
         <FormLabel>Admin Email</FormLabel>
         <Input
           name="email"
           value={formik.values.email}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
       </FormControl>
 
       <Button type="submit" colorScheme="blue">
