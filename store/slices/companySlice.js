2f46307419c8d98e59dc7c6aec9425baa643ad2f
Michael Desanker <mdesanker@gmail.com>
2022-04-06 20:18:08 -0400

    Add initial companySlice reducers

--- store/slices/companySlice.js
+++ store/slices/companySlice.js
@@ -1,14 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  isNew: false,
+  details: null,
+};
 
 const companySlice = createSlice({
   name: "company",
   initialState,
-  reducers: {},
+  reducers: {
+    setNew: (state) => {
+      state.isNew = true;
+    },
+    setReturning: (state) => {
+      state.isNew = false;
+    },
+    updateDetails: (state, { payload }) => {
+      state.details = payload;
+    },
+    companyReset: (state) => {
+      state.isNew = false;
+      state.details = null;
+    },
+  },
   extraReducers: (builder) => {},
 });
 
-// export const {} = companySlice.actions;
+export const { setNew, setReturning, updateDetails, companyReset } =
+  companySlice.actions;
 
 export default companySlice.reducer;
