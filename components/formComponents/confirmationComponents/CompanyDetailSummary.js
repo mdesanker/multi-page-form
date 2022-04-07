67fce7413db1ffa390c68389712260c0bad48785
Michael Desanker <mdesanker@gmail.com>
2022-04-07 05:53:00 -0400

    Reduce company summary for returning company

--- components/formComponents/confirmationComponents/CompanyDetailSummary.js
+++ components/formComponents/confirmationComponents/CompanyDetailSummary.js
@@ -1,17 +1,24 @@
 import { Heading, Text, VStack } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 import TextOutput from "./TextOutput";
 
 const CompanyDetailSummary = ({ data }) => {
+  const { isNew } = useSelector((state) => state.company);
+
   return (
     <VStack w="full" alignItems="flex-start" p={2}>
       <TextOutput label="Company ID" value={data.companyId} />
-      <TextOutput label="Company Name" value={data.name} />
-      <TextOutput label="Location" value={data.location} />
-      <TextOutput label="Remote Policy" value={data.policy} />
-      <TextOutput label="Company Size" value={data.size} />
-      <TextOutput label="Funding Stage" value={data.stage} />
-      <TextOutput label="Company URL" value={data.url} />
-      <TextOutput label="Admin Name" value={data.name} />
+      {isNew && (
+        <>
+          <TextOutput label="Company Name" value={data.name} />
+          <TextOutput label="Location" value={data.location} />
+          <TextOutput label="Remote Policy" value={data.policy} />
+          <TextOutput label="Company Size" value={data.size} />
+          <TextOutput label="Funding Stage" value={data.stage} />
+          <TextOutput label="Company URL" value={data.url} />
+          <TextOutput label="Admin Name" value={data.name} />
+        </>
+      )}
       <TextOutput label="Admin Email" value={data.email} />
     </VStack>
   );
