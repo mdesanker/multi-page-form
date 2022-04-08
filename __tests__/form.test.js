f092f976bcee0171aa281e9e38af8196c9b2980a
Michael Desanker <mdesanker@gmail.com>
2022-04-08 13:10:08 -0400

    Test that form inputs can be used

--- __tests__/form.test.js
+++ __tests__/form.test.js
@@ -16,6 +16,38 @@ describe("Form", () => {
     expect(screen.getAllByRole("combobox")).toHaveLength(3);
   });
 
+  it("typing into text inputs udpates them", () => {
+    render(<Form />);
+
+    const comapanyIdInput = screen.getByLabelText(/Company ID/i);
+    const companyNameInput = screen.getByLabelText(/Company Name/i);
+    const locationInput = screen.getByLabelText(/Location/i);
+    const policyInput = screen.getByLabelText(/Remote Policy/i);
+    const sizeInput = screen.getByLabelText(/Company Size/i);
+    const stageInput = screen.getByLabelText(/Funding Stage/i);
+    const urlInput = screen.getByLabelText(/Company Website/i);
+    const adminInput = screen.getByLabelText(/Admin Name/i);
+    const emailInput = screen.getByLabelText(/Admin Email/i);
+
+    expect(comapanyIdInput).toBeInTheDocument();
+    expect(companyNameInput).toBeInTheDocument();
+    expect(locationInput).toBeInTheDocument();
+    expect(policyInput).toBeInTheDocument();
+    expect(sizeInput).toBeInTheDocument();
+    expect(stageInput).toBeInTheDocument();
+    expect(urlInput).toBeInTheDocument();
+    expect(adminInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+
+    // Update a text field and a select dropdown
+    fireEvent.change(comapanyIdInput, { target: { value: "aa9999" } });
+    fireEvent.change(policyInput, { target: { value: 0 } });
+
+    expect(comapanyIdInput).toHaveValue("aa9999");
+    expect(policyInput[0].selected).toBeTruthy();
+    expect(policyInput[1].selected).toBeFalsy();
+  });
+
   it("renders returning company page version of first page", async () => {
     render(<Form />);
 
@@ -28,32 +60,3 @@ describe("Form", () => {
     expect(screen.getAllByRole("textbox")).toHaveLength(2);
   });
 });
-
-// const pageHeading = screen.getByRole("heading", {
-//   name: /Propose a Bounty/i,
-// });
-// const formHeading = screen.getByRole("heading", {
-//   name: /Company Details/i,
-// });
-// const comapanyIdInput = screen.getByLabelText(/Company ID/i);
-// const companyNameInput = screen.getByLabelText(/Company Name/i);
-// const locationInput = screen.getByLabelText(/Location/i);
-// const policyInput = screen.getByLabelText(/Remote Policy/i);
-// const sizeInput = screen.getByLabelText(/Company Size/i);
-// const stageInput = screen.getByLabelText(/Funding Stage/i);
-// const urlInput = screen.getByLabelText(/Company Website/i);
-// const adminInput = screen.getByLabelText(/Admin Name/i);
-// const emailInput = screen.getByLabelText(/Admin Email/i);
-
-// // screen.debug();
-// expect(pageHeading).toBeInTheDocument();
-// expect(formHeading).toBeInTheDocument();
-// expect(comapanyIdInput).toBeInTheDocument();
-// expect(companyNameInput).toBeInTheDocument();
-// expect(locationInput).toBeInTheDocument();
-// expect(policyInput).toBeInTheDocument();
-// expect(sizeInput).toBeInTheDocument();
-// expect(stageInput).toBeInTheDocument();
-// expect(urlInput).toBeInTheDocument();
-// expect(adminInput).toBeInTheDocument();
-// expect(emailInput).toBeInTheDocument();
