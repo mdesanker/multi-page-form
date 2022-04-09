83d59b4e588806fc87d0b2b7ae2622d769f2be03
Michael Desanker <mdesanker@gmail.com>
2022-04-09 10:18:26 -0400

    Hide referral btn on small screens

--- components/general/Header.js
+++ components/general/Header.js
@@ -5,21 +5,23 @@ const Header = () => {
   return (
     <Box as="header" w={"full"} h={20} bg="white" p={5}>
       <Flex justifyContent={"space-between"} alignItems="center" h="full">
-        <Flex alignItems={"center"} gap={2}>
-          <Image
-            src="/images/talentdrop_logo.png"
-            alt="Talentdrop logo"
-            boxSize="24px"
-            objectFit="cover"
-          />
-          <NextLink href="/" passHref>
-            <Heading as="h1" size={"md"} fontWeight="bold" cursor="pointer">
+        <NextLink href="/" passHref>
+          <Flex alignItems={"center"} gap={2} cursor="pointer">
+            <Image
+              src="/images/talentdrop_logo.png"
+              alt="Talentdrop logo"
+              boxSize="24px"
+              objectFit="cover"
+            />
+            <Heading as="h1" size={"md"} fontWeight="bold">
               Talentdrop
             </Heading>
-          </NextLink>
-        </Flex>
+          </Flex>
+        </NextLink>
         <Flex alignItems="center" gap={3}>
-          <Button colorScheme="blue">General Referral</Button>
+          <Button colorScheme="blue" display={["none", "block"]}>
+            General Referral
+          </Button>
           <Avatar bg="gray.400" boxSize="32px" cursor="pointer" />
         </Flex>
       </Flex>
