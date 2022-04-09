eede868da30f6078f5f981676ff73af6f3edc703
Michael Desanker <mdesanker@gmail.com>
2022-04-09 11:06:34 -0400

    Reset store on home nav click

--- components/general/Header.js
+++ components/general/Header.js
@@ -1,13 +1,28 @@
 import { Avatar, Box, Flex, Heading, Button } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { AiOutlineForm } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { companyReset } from "../../store/slices/companySlice";
+import { rolesReset } from "../../store/slices/roleSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const navHomeHandler = () => {
+    dispatch(companyReset());
+    dispatch(rolesReset());
+  };
+
   return (
     <Box as="header" w={"full"} h={20} bg="white" p={5}>
       <Flex justifyContent={"space-between"} alignItems="center" h="full">
         <NextLink href="/" passHref>
-          <Flex alignItems={"center"} gap={2} cursor="pointer">
+          <Flex
+            alignItems={"center"}
+            gap={2}
+            cursor="pointer"
+            onClick={navHomeHandler}
+          >
             <Box as={AiOutlineForm} h={8} w={8} color="blue.600" />
             <Heading as="h1" size={"md"} fontWeight="bold">
               Multi-Page Form
