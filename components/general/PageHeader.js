9ca4d8c0c7c0f0fd3714492c87123a19f4dd3805
Michael Desanker <mdesanker@gmail.com>
2022-04-10 04:57:46 -0400

    Add horizontal padding page header

--- components/general/PageHeader.js
+++ components/general/PageHeader.js
@@ -6,7 +6,7 @@ const PageHeader = () => {
       <Heading mt={4} textAlign="center">
         Propose a Bounty
       </Heading>
-      <Text mb={4} textAlign="center">
+      <Text mb={4} textAlign="center" px={4}>
         Talentdrop reviews and approves new bounty companies on a rolling basis.{" "}
         <Text as="span" fontWeight="semibold" color="blue.500" cursor="pointer">
           Learn more.
