cf3f4aed64d4f58e965f328c4ccedf0102fab6c9
Michael Desanker <mdesanker@gmail.com>
2022-04-10 06:00:50 -0400

    Update stepper style

--- components/stepCounter/Step.js
+++ components/stepCounter/Step.js
@@ -1,30 +1,28 @@
-import { Box, HStack, Square, Text } from "@chakra-ui/react";
+import { Box, Circle, Flex, Text } from "@chakra-ui/react";
 import { AiOutlineCheck } from "react-icons/ai";
 
 const Step = ({ step, title, active }) => {
+  const isSeen = step <= active + 1;
+  const isPassed = step <= active;
   const isActive = step === active + 1;
 
   return (
-    <HStack>
-      <Square
-        bg={isActive ? "blue.500" : "gray.100"}
-        h={12}
-        w={12}
-        rounded="md"
-        fontSize="2xl"
-        fontWeight="bold"
-        color={isActive ? "white" : "gray.500"}
-      >
-        {step > active ? step : <Box as={AiOutlineCheck} />}
-      </Square>
-      <Text
-        color={isActive ? "gray.800" : "gray.600"}
-        fontWeight="semibold"
-        display={["none", "block"]}
-      >
+    <Flex direction="column" alignItems="center">
+      <Text fontWeight="semibold" color={isSeen ? "blue.500" : "gray.400"}>
         {title}
       </Text>
-    </HStack>
+      <Circle
+        h={12}
+        w={12}
+        border="4px"
+        borderColor={isSeen ? "blue.500" : "gray.200"}
+        fontSize="2xl"
+        bg={isPassed ? "blue.500" : isActive ? "white" : "gray.200"}
+        // bg={isPassed ? "blue.500" : "gray.300"}
+      >
+        {isPassed && <Box as={AiOutlineCheck} color="white" />}
+      </Circle>
+    </Flex>
   );
 };
 
--- components/stepCounter/StepCounter.js
+++ components/stepCounter/StepCounter.js
@@ -3,18 +3,20 @@ import Step from "./Step";
 
 const StepCounter = ({ active }) => {
   return (
-    <HStack
-      w="full"
-      pb={4}
-      mb={4}
-      justifyContent="space-between"
-      borderBottom="1px"
-      borderBottomColor="gray.300"
-    >
-      <Step step={1} title="Company" active={active} />
-      <Step step={2} title="Roles" active={active} />
-      <Step step={3} title="Confirm" active={active} />
-    </HStack>
+    <>
+      <HStack
+        w="full"
+        pb={4}
+        mb={4}
+        justifyContent={["space-between", "space-around"]}
+        borderBottom="1px"
+        borderBottomColor="gray.300"
+      >
+        <Step step={1} title="Company" active={active} />
+        <Step step={2} title="Roles" active={active} />
+        <Step step={3} title="Confirm" active={active} />
+      </HStack>
+    </>
   );
 };
 
